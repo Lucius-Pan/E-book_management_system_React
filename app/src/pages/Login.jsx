54a0fe83c38a297c5e24f4362db2cc2e1340db4a
Lucius-Pan <lucius.indi@gmail.com>
2024-01-22 11:38:25 +0800

    实现登陆功能 Implement login function

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -2,43 +2,53 @@ import React from "react";
 import {Button, Col, Form, Input, Row} from "antd";
 import Title from "antd/es/skeleton/Title";
 import {useForm} from "antd/es/form/Form";
-
+import axios from "axios";
 
 
 const Login = () => {
     let [loginForm] = Form.useForm();
     const login = (values) => {
-        console.log(values);
+        console.log(values)
+        axios.post("/login", {
+                userName: values.userName,
+                passWord: values.passWord
+            }
+            ).then((res) => {
+            console.log(res.data);
+            }
+        ).catch((err) => {
+            console.log(err);
+        })
     }
     return (
         <>
             <div>
-               <Form onFinish={login} form={loginForm}>
-                   <Row gutter={24}>
-                       <Col style={{margin : "auto"}} span={3}>
-                           <h1>Login</h1>
-                       </Col>
-                   </Row>
-                   <Row gutter={24}>
-                       <Col style={{margin : "auto"}} span={6}>
-                           <Form.Item  label={"用户名"} labelAlign={"right"} name={"userName"}>
-                               <Input placeholder="Enter your username"/>
-                           </Form.Item>
-                       </Col>
-                   </Row>
-                   <Row gutter={24}>
-                       <Col style={{margin : "auto"}} span={6}>
-                           <Form.Item label={"密码"} labelAlign={"right"} name={"passWord"}>
-                               <Input placeholder="Enter your passWord"/>
-                           </Form.Item>
-                       </Col>
-                   </Row>
-                   <Row gutter={24}>
-                       <Col style={{margin :"auto"}}>
-                           <Button type={"primary"} htmlType={"submit"}>登陆</Button>
-                       </Col>
-                   </Row>
-               </Form>
+                <Form onFinish={login} form={loginForm}>
+                    <Row gutter={24}>
+                        <Col style={{margin: "auto"}} span={3}>
+                            <h1>Login</h1>
+                        </Col>
+                    </Row>
+                    <Row gutter={24}>
+                        <Col style={{margin: "auto"}} span={6}>
+                            <Form.Item label={"用户名"} labelAlign={"right"} name={"userName"}>
+                                <Input placeholder="Enter your username"/>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={24}>
+                        <Col style={{margin: "auto"}} span={6}>
+                            <Form.Item label={"密码"} labelAlign={"right"} name={"passWord"}>
+                                <Input placeholder="Enter your passWord"/>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={24}>
+                        <Col style={{margin: "auto"}}>
+                            <Button type={"primary"} htmlType={"submit"}>登陆</Button>
+                        </Col>
+                    </Row>
+                </Form>
 
             </div>
         </>
