61dd81f9edc8a01d8fdfce1989dcc363e64e2284
LuciusPan <lucius.indi@gmail.com>
2024-03-06 16:16:51 +0800

    Auto stash before rebase of "refs/heads/main"
    123123

--- app/src/components/CrvViewMotorVehicleInvoice.jsx
+++ app/src/components/CrvViewMotorVehicleInvoice.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import {Col, Drawer, Modal, Row} from "antd";
 
 
-const CrvViewMotorVehicleInvoice = () => {
-  return (
-    <>
-      <div>CrvViewMotorVehicleInvoice</div>
-    </>
-  );
+const CrvViewMotorVehicleInvoice = (props) => {
+    return (
+        <>
+            <Drawer
+            width={props.width || "100%"}
+            height={props.height || "100%"}
+            visible={props.visible}
+            title={props.title }
+            onClose={props.onCancel}
+            footer={props.footer }
+            >
+                <div>
+                    <Row>
+                        <span>发票预览</span>
+                    </Row>
+                    <Row gutter={8}>
+                        <Col span={6}>
+
+                        </Col>
+                        <Col span={6}>
+                            <span color={"#916447"}>机动车销售统一发票</span>
+                        </Col>
+                        <Col>
+
+                        </Col>
+                    </Row>
+                </div>
+            </Drawer>
+        </>
+    );
 
 }
 export default CrvViewMotorVehicleInvoice;
